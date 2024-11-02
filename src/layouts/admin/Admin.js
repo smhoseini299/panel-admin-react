0231dcd978d99b1d102634c5de9c22a188470cf0
smhoseini <smhoseini299@gmail.com>
2024-11-02 16:17:14 +0330

    Category & Content

--- src/layouts/admin/Admin.js
+++ src/layouts/admin/Admin.js
@@ -1,46 +1,20 @@
 import React, { useEffect } from 'react';
 import Header from './Header/Header'
 import Sadebar from "./Sidebar/Sadebar";
-import Dashboard from '../../page/dashboard/Dashboard';
+import Content from '../../page/Content';
+
 
 
 const Admin = () => {
 
-    useEffect(() => {
-        document.getElementById('handle_toggle_sidemenu').addEventListener('change', function () {
-            if (this.checked) {
-                document.querySelector('.mini_sidebar').classList.add('expanded')
-                document.getElementById('content_section').classList.add('with_sidebar')
-            } else {
-                document.querySelector('.mini_sidebar').classList.remove('expanded')
-                document.getElementById('content_section').classList.remove('with_sidebar')
-            }
-        })
 
-        // let sidebarItms = document.querySelectorAll('.sidebar_menu_item');
-        // for (const item of sidebarItms) {
-        //     item.addEventListener('click', () => {
-        //         for (const i of sidebarItms) {
-        //             i.classList.remove('active')
-        //         }
-        //         item.classList.add('active')
-        //         let sectionId = item.getAttribute('data-section-id');
-        //         let allSections = document.querySelectorAll('.main_section');
-        //         for (const section of allSections) {
-        //             section.classList.add('d-none')
-        //         }
-        //         document.getElementById(`${sectionId}`).classList.remove('d-none')
-        //     })
-        // }
-    }, [])
 
     return (
         <div>
+            <Content/>
             <Header />
             <Sadebar />
-            <section id="content_section" className={`bg-light py-2 px-3 `}>
-                <Dashboard/>
-            </section>
+           
         </div>
     );
 };
---
+++ src/page/Content.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import Category from "./category/Category";
+
+
+export default function Content() {
+
+    useEffect(() => {
+        document.getElementById('handle_toggle_sidemenu').addEventListener('change', function () {
+            if (this.checked) {
+                document.querySelector('.mini_sidebar').classList.add('expanded')
+                document.getElementById('content_section').classList.add('with_sidebar')
+            } else {
+                document.querySelector('.mini_sidebar').classList.remove('expanded')
+                document.getElementById('content_section').classList.remove('with_sidebar')
+            }
+        })
+    }, [])
+
+    return (
+        <section id="content_section" className={`bg-light py-2 px-3 `}>
+            {/* <Dashboard/> */}
+           <Category/>
+        </section>
+    )
+}
